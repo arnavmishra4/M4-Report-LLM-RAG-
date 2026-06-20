fde1ab6e602f3ab3981b834e98d061693e22a818
arnavmishra4 <139228867+arnavmishra4@users.noreply.github.com>
2026-06-21 04:04:17 +0530

    Remove hardcoded GEMINI_API_KEY
    Removed hardcoded API key and set it to an empty string.

--- llm.py
+++ llm.py
@@ -1,6 +1,6 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-os.environ["GEMINI_API_KEY"] = "AIzaSyBcFWDGPOSAjO7VMqJe40nIjXAAdwdfdzM"
+os.environ["GEMINI_API_KEY"] = ""
 
 llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
